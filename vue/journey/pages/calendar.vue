183c4404d8c14d294d4f079cee1c3a03500af9f8
Dominic Minischetti <dominic_minischetti@anfcorp.com>
2023-12-31 17:29:55 -0800

    Update calendar

--- vue/journey/pages/calendar.vue
+++ vue/journey/pages/calendar.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
-        <Calendar/>
+        <!-- Select for day, week, month, year views -->
+        <select v-model="view">
+            <option value="Day">Day</option>
+            <option value="Week">Week</option>
+            <option value="Month">Month</option>
+            <option value="Year">Year</option>
+        </select>
+
+        <!-- Calendar -->
+        <Calendar view="{{ view  }}"/>
     </div>
 </template>
+<script setup lang="ts">
+    import { ref } from "vue";
+    enum CalendarView {
+        Day = "Day",
+        Week = "Week",
+        Month = "Month",
+        Year = "Year",
+    }
+    const view = ref(CalendarView.Month);
+</script>
