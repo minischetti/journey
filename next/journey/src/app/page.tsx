b252a26c632e28d33558395ba7de49b8bd49ae29
Dominic Minischetti <dominic_minischetti@anfcorp.com>
2024-01-12 18:55:33 -0800

    Checkpoint

--- next/journey/src/app/page.tsx
+++ next/journey/src/app/page.tsx
@@ -4,7 +4,24 @@ import Calendar from "./Calendar";
 import * as Types from "./types";
 import { Command } from "cmdk";
 import { ItemsContext, ItemsProvider } from "./context";
-import { Tag as TagIcon, Plus } from "@phosphor-icons/react";
+import { Tag as TagIcon, Plus, X } from "@phosphor-icons/react";
+
+const AppContext = createContext({});
+const { Provider } = AppContext;
+
+const AppProvider = ({ children }: { children: JSX.Element }) => {
+    const [items, setItems] = useState<Types.Item[]>([]);
+
+    return <Provider value={{ items, setItems }}>{children}</Provider>;
+};
+
+enum Status {
+    backlog = "backlog",
+    upcoming = "upcoming",
+    inProgress = "inProgress",
+    cancelled = "cancelled",
+    completed = "completed",
+}
 
 const Popup = ({ children }: { children: JSX.Element }) => {
     return (
@@ -38,25 +55,60 @@ const Accordion = ({ title, children }: { title: string; children: JSX.Element }
     );
 };
 
-function Input({ name, placeholder, onChange }: { name: string; placeholder: string; onChange: (e: any) => void }) {
+function Input({ label, name, placeholder, onChange }: { label: string; name: string; placeholder: string; onChange: (e: any) => void }) {
     return (
-        <input
-            name={name}
-            placeholder={placeholder}
-            onChange={onChange}
-            className="border bg-zinc-800 border-zinc-700 focus:outline-zinc-800 outline-none rounded-md p-2 appearance-none"
-        />
+        <div className="flex flex-col gap-1">
+            <Label>{label}</Label>
+            <input
+                name={name}
+                placeholder={placeholder}
+                onChange={onChange}
+                className="border bg-zinc-800 border-zinc-700 focus:outline-zinc-800 outline-none rounded-md p-2 appearance-none"
+            />
+        </div>
+    );
+}
+
+function Label({ children }: { children: JSX.Element }) {
+    return <label className="text-xs uppercase font-bold">{children}</label>;
+}
+
+function Select({ label, name, children }: { label: string; name: string; children: JSX.Element[] }) {
+    return (
+        <div className="flex flex-col gap-1">
+            <Label>{label}</Label>
+            <select
+                name={name}
+                value={Status.backlog}
+                className="border bg-zinc-800 border-zinc-700 focus:outline-zinc-800 outline-none rounded-md p-2 appearance-none">
+                {children}
+            </select>
+        </div>
     );
 }
 
 function Item({ name, description, status, items, tags }: Types.Item) {
     return (
         <div className="border border-gray-400 rounded-md p-2">
-            <h3>{name}</h3>
-            <p>{description}</p>
+            <div className="flex justify-between">
+                <div className="flex gap-2">
+                    <TagIcon weight="bold" />
+                    <Plus weight="bold" />
+                    <X weight="bold" />
+                </div>
+            </div>
+            <h2 contentEditable>{name}</h2>
+            <p contentEditable>{description}</p>
             <p>{status}</p>
             {items && items.map((item, itemIndex) => <Item key={itemIndex} {...item} />)}
-            {tags && tags.map((tag, tagIndex) => <Tag key={tagIndex} name={tag} />)}
+            {tags && (
+                <Tags>
+                    <Label>Tags</Label>
+                    {tags.map((tag, tagIndex) => (
+                        <Tag key={tagIndex} name={tag} />
+                    ))}
+                </Tags>
+            )}
         </div>
     );
 }
@@ -143,6 +195,9 @@ function App() {
     const updateTags = (e: any) => {
         e.preventDefault();
         const tag = e.target.value;
+        if (!tag) {
+            return;
+        }
         const lastChar = tag.slice(-1);
         const isComma = lastChar === ",";
         const isSpace = lastChar === " ";
@@ -203,33 +258,40 @@ function App() {
                         ))}
                 </Accordion>
             ))} */}
-                <Plus />
                 <div className="flex flex-col gap-2 p-2 rounded-md bg-zinc-800">
                     <form onSubmit={formSubmit} className="flex flex-col gap-2">
-                        <Input name="name" placeholder="Enter an item name..." onChange={(e) => setComposeTitle(e.target.value)} />
-                        {/* <Popup>
+                        <Input label="name" placeholder="Enter an item name..." />
+                        <Accordion title="More...">
+                            {/* <Popup>
                         <div className="flex flex-col gap-2">
                             <h2>{composeTitle}</h2>
                             <p>{composeTags}</p>
-                            <button onClick={closeItem}>Close</button>
+                            <button>Close</button>
                         </div>
                     </Popup> */}
-                        <div className="flex gap-2 p-2 rounded-md bg-zinc-800 items-center text-sm">
-                            {composeTags.map((tag, tagIndex) => (
-                                <span className="py-1 px-3 flex gap-1 rounded-md bg-zinc-600 items-center" key={tagIndex}>
-                                    <span>{tag}</span>
-                                    <span className="cursor-pointer" onClick={removeTag(tagIndex)}>
-                                        x
+                            <Input label="description" placeholder="Enter a description..." />
+                            <Select label="status" name="status">
+                                {Object.values(Status).map((status, statusIndex) => (
+                                    <option key={statusIndex} value={status}>
+                                        {status}
+                                    </option>
+                                ))}
+                            </Select>
+                            <div className="flex gap-2 p-2 rounded-md bg-zinc-800 items-center text-sm">
+                                {composeTags.map((tag, tagIndex) => (
+                                    <span className="py-1 px-3 flex gap-1 rounded-md bg-zinc-600 items-center" key={tagIndex}>
+                                        <span>{tag}</span>
+                                        <span className="cursor-pointer" onClick={removeTag(tagIndex)}>
+                                            <X weight="bold" />
+                                        </span>
                                     </span>
-                                </span>
-                            ))}
-                            {/* Clear the input when the user presses enter */}
-                            <Input name="tags" placeholder="Enter tags..." onChange={updateTags} />
-                        </div>
-                        <Button type="submit">
+                                ))}
+                                <Input name="tags" placeholder="Enter tags..." onChange={updateTags} />
+                            </div>
+                        </Accordion>
+                        <Button type="submit" variant={Variants.circle}>
                             <span className="flex gap-1 items-center">
-                                <span>Add</span>
-                                <Plus />
+                                <Plus weight="bold" />
                             </span>
                         </Button>
                     </form>
@@ -244,9 +306,26 @@ function App() {
     );
 }
 
-function Button({ type, children, onClick }: { type: string; children: JSX.Element; onClick: () => void }) {
+enum Variants {
+    default = "rounded-md",
+    circle = "rounded-full",
+    outline = "border border-zinc-600",
+    underline = "border-b border-zinc-600",
+    ghost = "bg-transparent",
+}
+function Button({
+    type,
+    children,
+    onClick,
+    variant = Variants.default,
+}: {
+    type: "button" | "submit";
+    children: JSX.Element;
+    onClick?: () => void;
+    variant?: Variants;
+}) {
     return (
-        <button type={type} onClick={onClick} className="bg-zinc-600 rounded-md p-2">
+        <button type={type} onClick={onClick} className={`flex gap-1 items-center p-2 bg-zinc-600 text-zinc-100 ${variant}`}>
             {children}
         </button>
     );
